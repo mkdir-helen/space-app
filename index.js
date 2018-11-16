69c078f4061629e41d8aaf38233d790589a038f9
Collin Argo <collinargo@gmail.com>
2018-11-16 13:18:23 -0500

    uncomment express static path

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post('/login', (req, res) => {
 
 
 //Connect to stylesheets
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 //Configure body-parser to read data sent by HTML form tags
 app.use(bodyParser.urlencoded({ extended: false }));
