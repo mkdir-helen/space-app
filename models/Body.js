65717676e15ed95c106d8da75aa3a0ee13ace9b2
Isaac Fonseca <isaacfo1010@gmail.com>
2018-11-16 13:15:29 -0500

    added some static methods to body.js, did not add body_type fully

--- models/Body.js
+++ models/Body.js
@@ -1,11 +1,38 @@
 const db = require('./db');
 
 class Body {
-    constructor(name) {
+    constructor(id, name, type = null) {
+        this.id = id;
         this.name = name;
+        this.type = type;
     }
 
-    
+    // Create
+    static add(name){
+        return db.one(`insert into bodies(name)
+        values($1) returning id`, [name])
+        .then(result => new body(result.id, name, type))
+    }
+
+// retrieve
+static getById(id) {
+    return db.one('select * from bodies where id=$1', [id])
+    .then(body => new body(body.id, body.name,))
 }
+
+static getByName(name) {
+    return db.any('select * from events where name=$1', [name])
+    .then(bodyArray => bodyArray.map(body => new body(body.id, body.name,)))
+}
+
+// update
+
+// delete
+delete() {
+    return db.result('delete from bodies where id=$1', [this.id])
+}
+}
+
+ 
 
 module.exports = Body
