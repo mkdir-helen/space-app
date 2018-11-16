91129eab42536284e9c29f5ea8f89d2cabfab113
mkdir-helen <experiment.helen@gmail.com>
2018-11-15 20:00:51 -0500

    Finally got it to google authenticate when logging in

--- auth.js
+++ auth.js
@@ -4,46 +4,84 @@ const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
-
-
-app.use(session({
-    secret: 'random1234',
-    resave: true,
-    saveUninitialized: true,
-}));
-
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
-    console.log(profile);
-  }
-));
+const User = require('./models/User');
 
 // app.use(cookieSession({
 //     maxAge:24*60*60*1000,
 //     keys: [process.env.cookieKey]
 // }));
 
+//set up session middleware
+app.use(session({
+    secret: 'random1234',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+passport.serializeUser((user,done)=> {
+    console.log(user.google_id);
+    console.log('this is serializeUser');
+    done(null, user.id); 
+})
+
+passport.deserializeUser((id, done)=>{
+    User.getUsers(id).then((user)=> {
+        done(null, user);
+    })
+})  
+//set up passport strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.getUsers(profile.id).then((currentUser) => {
+        if(currentUser){
+            //already have the user
+            done(null, currentUser);
+            console.log('Currently logged in?');
+        }else {
+            //if not, create user in our db
+            User.oAdd(
+                profile.displayName,
+                profile.id,
+                profile._json.image.url
+            ).then((newUser) => {
+                console.log(newUser);
+                console.log('this is the newUser');
+                done(null, newUser);
+            });
+        }
+    })
+    }
+    ));
+    
+
 //initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', (req,res) =>{
-
+    res.send('home');
 })
+
+app.get('/login', passport.authenticate('google', {
+    scope:
+    ['https://www.googleapis.com/auth/userinfo.profile', ],
+    accessType: 'offline',
+    prompt: 'consent',
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+    }));
+
+    
 
 app.get('/logout', (req,res)=> {
-
+    res.logout();
+    res.redirect('/');
 })
 
-app.get('/login', passport.authenticate('google', { scope: ['profile'] }));
 
 // app.get('/auth/google',
 //   passport.authenticate('google', { scope: ['profile'] }));
@@ -52,17 +90,12 @@ app.get('/auth/google',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect('/profile');
   });
 
-// passport.serializeUser((user,done)=> {
-//     // done(null, user.id); 
-// })
+app.get('/profile', (req, res)=>{
+    res.send('profile');
+});  
 
-// passport.deserializeUser((id, done)=>{
-//     User.findById(id).then((user)=> {
-//         // done(null, user);
-//     })
-// })  
 
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,10 +1,50 @@
 const db = require('./db');
 
 class User {
-    constructor() {
-
+    constructor(id, name, google_ID, thumbnail) {
+        // define properties that
+        // are also the names
+        // of the database columns
+        this.id = id;
+        this.name = name;
+        // this.location = location;
+        this.google_ID = google_ID;
+        // this.username = username;
+        this.thumbnail = thumbnail;
     }
 
+    // CREATE
+    static oAdd(name, google_ID, thumbnail) {
+        return db.one(`
+            insert into users 
+                (name, google_ID, thumbnail)
+            values
+                ($1, $2, $3)
+            returning id    
+            `, [name, google_ID, thumbnail])
+            .then(data => {
+                const u = new User(data.id, name, google_ID, thumbnail);
+                return u;
+            });
+    }
+
+        // RETRIEVE
+        static getUsers(gid) {
+            return db.one(`
+                select * from users where google_ID=$1
+            `,[gid]).then(userObj => {
+                // transform array of objects
+                // into array of User instances
+                console.log(userObj);
+                console.log('userObj in getUsers');
+                    const u = new User(userObj.id, userObj.name, userObj.google_ID, userObj.thumbnail);
+                    return u;
+            }).catch(
+                () => {
+                    return false;
+                }
+            )
+        }
 
 }
 
