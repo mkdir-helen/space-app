3001cae4ea5effe7c296c9d9fe4f382dd7f80066
Collin Argo <collinargo@gmail.com>
2018-11-19 17:56:27 -0500

    reorder getAll new User creation call

--- models/User.js
+++ models/User.js
@@ -59,7 +59,7 @@ static getAll() {
         // transform array of objects
         // into array of User instances
         const instanceArray = userArray.map(userObj => {
-            const u = new User(userObj.id, userObj.name, null, null, userObj.lat, userObj.long, userObj.username, userObj.pwhash);
+            const u = new User(userObj.id, userObj.name, userObj.lat, userObj.long, userObj.username, userObj.pwhash null, null,  );
             return u;
         });
         return instanceArray;
