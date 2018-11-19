7e0774fbbc913c8c7f89b27bf162da3d1b54aab6
Collin Argo <collinargo@gmail.com>
2018-11-19 17:40:40 -0500

    comment out fetch calls

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ function updateEvents() {
     // User.getLocation()
     // .then(fetchClouds)
     // get weather forecast
-    fetchClouds([37.8267, -122.4233])
-    fetchSpace()
-    fetchDoomsday()
+    // fetchClouds([37.8267, -122.4233])
+    // fetchSpace()
+    // fetchDoomsday()
 }
 
 updateEvents() 
