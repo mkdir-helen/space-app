af4284f9e5643c8b37c39236c139f79d13f97d63
mkdir-helen <experiment.helen@gmail.com>
2018-11-20 14:32:24 -0500

    Added buttons to about page and restyled the profile buttons

--- views/about.js
+++ views/about.js
@@ -7,13 +7,33 @@ function about() {
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <link rel="stylesheet" href="../css/style.css">
+       <link rel="stylesheet" href="../css/about.css">
+       <link rel="stylesheet" href="../css/myspace.css">
        <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0"
            ; crossorigin="anonymous"></script>
        <title>AboutUS</title>
    </head>
    
    <body>
+   <div class="wrapper">
+   <!-- Navigation -->
+   <nav class="main-nav">
+       <ul>
+           <li>
+               <a href="/events">Home <i class="fas fa-globe"></i></a>
+
+           </li>
+           <li>
+               <a href="/profile">My Space <i class="fas fa-globe"></i></a>
+           </li>
+           <li>
+               <a href="#">Login <i class="fas fa-sign-in-alt"></i></a>
+           </li>
+           <li>
+               <a href="#">SignUp <i class="fas fa-user-plus"></i></a>
+           </li>
+       </ul>
+   </nav>
            <section class="cf team-container">
                <h1 class="team-h1">Our Team</h1>
    
@@ -34,7 +54,7 @@ function about() {
    
                <!-- member-->
                <div class="team-member">
-                   <img class="team-photo" src="img/fullsizeoutput_ec4.jpeg">
+                   <img class="team-photo" src="/img/fullsizeoutput_ec4.jpeg">
                    <h3>Colin Argo</h3>
    
                </div>
@@ -43,7 +63,7 @@ function about() {
    
                <!-- member-->
                <div class="team-member">
-                   <img class="team-photo" src="/space-app/front-end/img/42783503.png">
+                   <img class="team-photo" src="/img/42783503.png">
                    <h3>Will Harris</h3>
    
                </div>
@@ -53,7 +73,7 @@ function about() {
    
            </section>
    
-   
+   </div>
    </body>
     `;
 }
