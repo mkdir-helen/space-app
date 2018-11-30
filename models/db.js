0a79b9b9caac2a65015a3438315757bf48d4ddf7
mkdir-helen <experiment.helen@gmail.com>
2018-11-30 16:31:20 -0500

    testing pgp

--- models/db.js
+++ models/db.js
@@ -1,18 +1,47 @@
+const initOptions = {
+    // global event notification;
+    error(error, e) {
+        if (e.cn) {
+            // A connection-related error;
+            //
+            // Connections are reported back with the password hashed,
+            // for safe errors logging, without exposing passwords.
+            console.log('CN:', e.cn);
+            console.log('EVENT:', error.message || error);
+        }
+    }
+};
 // ============================================
 // Database Connection
 const pgp = require('pg-promise')({
-    query: e => {      
+    query: e => {
         console.log('QUERY: ', e.query);
         if (e.params) {
             console.log('PARAMS:', e.params);
-        }       
+        }
     }
 });
+// Testing Connection
+const pgpTwo = require('pg-promise')(initOptions);
+
+
+
 const db = pgp({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
 });
 // ============================================
+
+// using an invalid connection string:
+const dbOne = pgpTwo('postgresql://ubuntu@' + host + ':' + port + '/' + database);
+
+dbOne.connect()
+    .then(obj => {
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
 
 module.exports = db;
